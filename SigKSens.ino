4d587bb9cb5327423b47b6c7b4e2e577286b2a90
Thomas St.Pierre <thomas@samoht.ca>
2017-10-16 23:05:14 -0400

    print pretty address

--- SigKSens.ino
+++ SigKSens.ino
@@ -261,9 +261,11 @@ void printAddress(DeviceAddress deviceAddress)
  for (uint8_t i = 0; i < 8; i++)
   {
     // zero pad the address if necessary
-    //if (deviceAddress[i] < 16) Serial.print("0");
+    if (deviceAddress[i] < 16) Serial.print("0");
     Serial.print(deviceAddress[i], HEX);
-    Serial.print("-");
+    if (i < 7) {
+      Serial.print(":");
+    }
   }
 }
 
