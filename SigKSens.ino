8b1dd76401ecba39de5673ce0fcb7e5aad4c3e7c
Thomas St.Pierre <thomas@samoht.ca>
2017-11-02 23:41:27 -0400

    cleanup

--- SigKSens.ino
+++ SigKSens.ino
@@ -25,9 +25,20 @@ Defines
 
 
 #define RESET_CONFIG_PIN 0
+
 #define ONE_WIRE_BUS 13   // D7 pin on ESP
-#define TEMPERATURE_PRECISION 10 // 10 is 0.25C resolution in 187ms. (11 is .125C Resolution in 375ms, 12 .0625C in 750ms)
+
+// set these together!
+// 9  is 0.5C in 94ms
+// 10 is 0.25C in 187ms
+// 11 is 0.125C in 375ms
+// 12 is 0.0625C in 750ms
+#define TEMPERATURE_PRECISION 9
+#define ONEWIRE_READ_DELAY 94
+
 #define MAX_SIGNALK_PATH_LEN 100
+
+
 
 /*---------------------------------------------------------------------------------------------------
 -----------------------------------------------------------------------------------------------------
@@ -163,6 +174,7 @@ void resetConfig() {
 }
 
 
+
 /*---------------------------------------------------------------------------------------------------
 -----------------------------------------------------------------------------------------------------
 One Wire
@@ -249,27 +261,13 @@ void request1WSensors() {
   sensors.requestTemperatures();
 
   // start ready timer
-  uint16_t msWait;
-  switch(TEMPERATURE_PRECISION) {
-    case 9:
-      msWait = 94;
-    case 10:
-      msWait =  188;
-    case 11:
-      msWait =  375;
-    default:
-      msWait =  750;
-  }
-  os_timer_arm(&oneWireReadyTimer, msWait, false); // false = no loop
+  os_timer_arm(&oneWireReadyTimer, ONEWIRE_READ_DELAY, false); // false = no loop
   
 }
 
-
-
-
 void read1WSensors() {
   readyToRead1Wire = false; // reset interupt
- 
+
   uint8_t tempDeviceAddress[8];
   int numberOfDevices = 0;
 
@@ -310,6 +308,90 @@ void read1WSensors() {
 
 
 
+/*---------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------
+HTTP
+-----------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------*/
+
+void setupHTTP() {
+  Serial.println("starting webserver");
+  server.onNotFound(handleNotFound);
+  server.serveStatic("/", SPIFFS, "/web/index.html");
+  server.serveStatic("/index.html", SPIFFS, "/web/index.html");
+  server.on("/getSensors", HTTP_GET, htmlGetSensors);
+  server.on("/set1wSensorPath", HTTP_GET, htmlSet1WPath);
+  server.on("/description.xml", HTTP_GET, [](){  SSDP.schema(server.client()); });
+  server.begin();
+}
+
+
+void handleNotFound(){
+  server.send(404, "text/plain", "404: Not found"); // Send HTTP status 404 (Not Found) when there's no handler for the URI in the request
+}
+
+void htmlGetSensors() {
+  DynamicJsonBuffer jsonBuffer;
+  SensorInfo *tmpSensorInfo;
+  char response[1024];
+  JsonObject& json = jsonBuffer.createObject();
+  char strAddress[32];
+  //sensors
+  JsonArray& oneWSensors = json.createNestedArray("1wSensors");
+  for (uint8_t i=0; i < sensorList.size(); i++) {
+    tmpSensorInfo = sensorList.get(i);
+    JsonObject& tmpSens = oneWSensors.createNestedObject();
+    sprintf(strAddress, "%02X:%02X:%02X:%02X:%02X:%02X:%02X:%02X", 
+        tmpSensorInfo->sensorAddress[0], 
+        tmpSensorInfo->sensorAddress[1], 
+        tmpSensorInfo->sensorAddress[2],
+        tmpSensorInfo->sensorAddress[3], 
+        tmpSensorInfo->sensorAddress[4], 
+        tmpSensorInfo->sensorAddress[5], 
+        tmpSensorInfo->sensorAddress[6], 
+        tmpSensorInfo->sensorAddress[7]  );
+    tmpSens.set<String>("address", strAddress);
+    tmpSens.set<String>("signalKPath", tmpSensorInfo->signalKPath );
+    tmpSens.set<float>("tempK", tmpSensorInfo->tempK);
+  }
+  json.printTo(response);
+  server.send ( 200, "application/json", response);
+}
+
+
+void htmlSet1WPath() {
+  
+  DeviceAddress address;
+  SensorInfo *tmpSensorInfo;
+  char pathStr[MAX_SIGNALK_PATH_LEN];
+  bool found = false;
+
+  Serial.print("Setting path for 1W Sensor: ");
+  if(!server.hasArg("address")) {server.send(500, "text/plain", "missing arg 'address'"); return;}
+  if(!server.hasArg("path")) {server.send(500, "text/plain", "missing arg 'path'"); return;}
+  
+  String addressStr = server.arg("address");
+  server.arg("path").toCharArray(pathStr, MAX_SIGNALK_PATH_LEN);
+
+  parseBytes(addressStr.c_str(), ':', address,  8, 16);
+
+  for (int x=0;x<sensorList.size() ; x++) {
+    tmpSensorInfo = sensorList.get(x);
+    if (memcmp(tmpSensorInfo->sensorAddress, address, sizeof(address)) == 0) {
+      memcpy(tmpSensorInfo->signalKPath, pathStr, MAX_SIGNALK_PATH_LEN);
+      found = true;
+      break; //no need to check others if we found it
+    }
+  }
+
+  if (found) {
+    saveConfig();
+    server.send(200, "application/json", "{ \"success\": true }");
+  } else {
+    server.send(500, "application/json", "{ \"success\": false }");
+  }
+  
+}
 
 
 
@@ -339,13 +421,11 @@ void read1WSensors() {
 
 
 
-
-
-
-
-
-
-
+/*---------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------
+General Setup
+-----------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------*/
 
 
 
@@ -435,20 +515,6 @@ void setupDiscovery() {
     SSDP.begin();
 }
 
-void setupHTTP() {
-  Serial.println("starting webserver");
-  server.onNotFound(handleNotFound);
-  server.serveStatic("/", SPIFFS, "/web/index.html");
-  server.serveStatic("/index.html", SPIFFS, "/web/index.html");
-  server.on("/getSensors", HTTP_GET, htmlGetSensors);
-  server.on("/set1wSensorPath", HTTP_GET, htmlSet1WPath);
-  server.on("/description.xml", HTTP_GET, [](){  SSDP.schema(server.client()); });
-  server.begin();
-}
-
-
-
-
 
 void setup() {
   // put your setup code here, to run once:
@@ -473,88 +539,24 @@ void setup() {
   
 }
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////// End Setup! /////////////////////////////////////////////////////////////////////////////////////////
-void handleNotFound(){
-  server.send(404, "text/plain", "404: Not found"); // Send HTTP status 404 (Not Found) when there's no handler for the URI in the request
-}
-
-void htmlGetSensors() {
-  DynamicJsonBuffer jsonBuffer;
-  SensorInfo *tmpSensorInfo;
-  char response[1024];
-  JsonObject& json = jsonBuffer.createObject();
-  char strAddress[32];
-  //sensors
-  JsonArray& oneWSensors = json.createNestedArray("1wSensors");
-  for (uint8_t i=0; i < sensorList.size(); i++) {
-    tmpSensorInfo = sensorList.get(i);
-    JsonObject& tmpSens = oneWSensors.createNestedObject();
-    sprintf(strAddress, "%02X:%02X:%02X:%02X:%02X:%02X:%02X:%02X", 
-        tmpSensorInfo->sensorAddress[0], 
-        tmpSensorInfo->sensorAddress[1], 
-        tmpSensorInfo->sensorAddress[2],
-        tmpSensorInfo->sensorAddress[3], 
-        tmpSensorInfo->sensorAddress[4], 
-        tmpSensorInfo->sensorAddress[5], 
-        tmpSensorInfo->sensorAddress[6], 
-        tmpSensorInfo->sensorAddress[7]  );
-    tmpSens.set<String>("address", strAddress);
-    tmpSens.set<String>("signalKPath", tmpSensorInfo->signalKPath );
-    tmpSens.set<float>("tempK", tmpSensorInfo->tempK);
-  }
-  json.printTo(response);
-  server.send ( 200, "application/json", response);
-}
-
-
-void htmlSet1WPath() {
-  
-  DeviceAddress address;
-  SensorInfo *tmpSensorInfo;
-  char pathStr[MAX_SIGNALK_PATH_LEN];
-  bool found = false;
-
-  Serial.print("Setting path for 1W Sensor: ");
-  if(!server.hasArg("address")) {server.send(500, "text/plain", "missing arg 'address'"); return;}
-  if(!server.hasArg("path")) {server.send(500, "text/plain", "missing arg 'path'"); return;}
-  
-  String addressStr = server.arg("address");
-  server.arg("path").toCharArray(pathStr, MAX_SIGNALK_PATH_LEN);
-
-  parseBytes(addressStr.c_str(), ':', address,  8, 16);
-
-  for (int x=0;x<sensorList.size() ; x++) {
-    tmpSensorInfo = sensorList.get(x);
-    if (memcmp(tmpSensorInfo->sensorAddress, address, sizeof(address)) == 0) {
-      memcpy(tmpSensorInfo->signalKPath, pathStr, MAX_SIGNALK_PATH_LEN);
-      found = true;
-      break; //no need to check others if we found it
-    }
-  }
-
-  if (found) {
-    saveConfig();
-    server.send(200, "application/json", "{ \"success\": true }");
-  } else {
-    server.send(500, "application/json", "{ \"success\": false }");
-  }
-  
-}
-
-
+/*---------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------
+Main Loop!
+-----------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------*/
 
 
 void loop() {
   //device mgmt
   ArduinoOTA.handle();
-  server.handleClient();
+  yield();           
+  
 
   //our stuff
   handle1Wire();
+  server.handleClient();
   
 
-  yield();           
 
   
 
@@ -565,6 +567,12 @@ void loop() {
   } 
 
 }
+
+/*---------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------
+Helper functions
+-----------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------*/
 
 
 void parseBytes(const char* str, char sep, byte* bytes, int maxBytes, int base) {
