9805b1cd7676e56e007326bb861502a968966241
Thomas St.Pierre <thomas@samoht.ca>
2019-02-16 20:05:39 -0500

    Update firmware over HTTP  :)

--- SigkSens/src/net/httpd.cpp
+++ SigkSens/src/net/httpd.cpp
@@ -11,6 +11,8 @@
   #include <AsyncTCP.h>
 #endif
 
+#include <Update.h>
+
 #include <ESPAsyncWebServer.h>
 #include <StreamString.h>
 #include <AsyncJson.h>
@@ -61,6 +63,7 @@ HTTP
 ----------------------------------------------------------------------------*/
 
 AsyncWebServer server(HTTP_PORT);
+bool shouldReboot = false;
 
 /* No static files for the moment
 void createStaticFiles() {
@@ -282,6 +285,39 @@ void setupHTTP() {
   server.on("/index.html",[](AsyncWebServerRequest *request ) {
       request->send_P(200, "text/html", INDEX_PAGE);
     });
+
+  // Simple Firmware Update Form
+  server.on("/update", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(200, "text/html", "<form method='POST' action='/update' enctype='multipart/form-data'><input type='file' name='update'><input type='submit' value='Update'></form>");
+  });
+  server.on("/update", HTTP_POST, [](AsyncWebServerRequest *request){
+    shouldReboot = !Update.hasError();
+    AsyncWebServerResponse *response = request->beginResponse(200, "text/plain", shouldReboot?"OK":"FAIL");
+    response->addHeader("Connection", "close");
+    request->send(response);
+  },[](AsyncWebServerRequest *request, String filename, size_t index, uint8_t *data, size_t len, bool final){
+    if(!index){
+      Serial.printf("Update Start: %s\n", filename.c_str());
+#ifdef ESP8266
+      Update.runAsync(true);
+#endif
+      if(!Update.begin((ESP.getFreeSketchSpace() - 0x1000) & 0xFFFFF000)){
+        Update.printError(Serial);
+      }
+    }
+    if(!Update.hasError()){
+      if(Update.write(data, len) != len){
+        Update.printError(Serial);
+      }
+    }
+    if(final){
+      if(Update.end(true)){
+        Serial.printf("Update Success: %uB\n", index+len);
+      } else {
+        Update.printError(Serial);
+      }
+    }
+  });
 
 
   server.on("/getSensorInfo", HTTP_GET, httpGetSensorInfo);
