334a85ce46f842c05b63934f5d4599f1d8d7ca45
Thomas St.Pierre <thomas@samoht.ca>
2019-04-03 22:41:32 -0400

    Fix channel

--- SigkSens/src/hardware/digitalSwitch.cpp
+++ SigkSens/src/hardware/digitalSwitch.cpp
@@ -3,6 +3,6 @@
 #include "../components/digitalSwitch/digitalSwitch.h"
 
 ComponentDigitalSwitch Channel1("Channel 1", 26, 72, 1);
-ComponentDigitalSwitch Channel2("Channel 2", 14, 72, 2);
+ComponentDigitalSwitch Channel2("Channel 2", 14, 72, 0);
 
 #endif
